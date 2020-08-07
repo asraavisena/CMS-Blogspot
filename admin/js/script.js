3fa66b42799403f1a8192029fe04c62012049333
asraavisena <asra.avisena@yahoo.com>
2020-08-07 19:34:34 +0200

    adding bulk option for publish draft and delete

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
     // EDITOR CKEDITOR
-    ClassicEditor
-    .create( document.querySelector( '#body' ) )
-    .catch( error => {
+    ClassicEditor.create( document.querySelector( '#body' ) ).catch( error => {
         console.error( error );
     } );
+
+    $('#selectAllBoxes').click(function(event){
+        if(this.checked){
+            $('.checkBoxes').each(function(){
+                this.checked = true;
+            })
+        } else {
+            $('.checkBoxes').each(function(){
+                this.checked = false;
+            })  
+        }
+    });
 
 
 });
